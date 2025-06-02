d874b160dbaa7f46ea76426e11347d668e843009
juliannot <fenonantenaikolovasoa@gmail.com>
2025-06-02 11:52:00 +0300

    v1.2.4.1

--- Assistant_Backend/config.py
+++ Assistant_Backend/config.py
@@ -3,7 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    # SQLALCHEMY_DATABASE_URI = 'mysql://orion:orion3691@localhost/db_assistant'
-    # SQLALCHEMY_DATABASE_URI = 'mysql://sql8781008:E4FLPaxM6v@sql8.freesqldatabase.com:3306/sql8781008'
     SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:npg_BHgmSX1Qxn9M@ep-long-boat-a27ai8pg-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
